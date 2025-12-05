b2a0a178abceb1dbc292d1b898d74782dacbd5cf
Julieta <julj.grosso16@gmail.com>
2025-12-05 13:57:12 -0300

    fix: release

--- instituciones.sql
+++ instituciones.sql
@@ -2,7 +2,7 @@
 -- Query: select * from institucion
 LIMIT 0, 1000
 
--- Date: 2025-12-05 13:49
+-- Date: 2025-12-05 13:49 
 */
 INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (1,'https://i.imgur.com/LNsGm0g.png','Biblioteca Popular JME','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','APP_USR-5234e05b-522b-4dca-bf5b-357f67b056ee');
 INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (2,'https://i.imgur.com/UNHP3jT.png','ISP N° 63 \"N.Q\"','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','djnksdn');
