4d4d801b38ac3673cc213a92c564e72ef12e74e0
ISP63-4 <julj.grosso16@gmail.com>
2025-12-05 13:50:38 -0300

    consultas

---
+++ instituciones.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select * from institucion
+LIMIT 0, 1000
+
+-- Date: 2025-12-05 13:49
+*/
+INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (1,'https://i.imgur.com/LNsGm0g.png','Biblioteca Popular JME','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','APP_USR-5234e05b-522b-4dca-bf5b-357f67b056ee');
+INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (2,'https://i.imgur.com/UNHP3jT.png','ISP N° 63 \"N.Q\"','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','djnksdn');
+INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (3,'https://i.imgur.com/WtiJ81a.png','Bomberos Solidarios LT','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','djnksdn');
+INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (4,'https://i.imgur.com/P9GOIa9.png','Asociación Huellitas','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','djnksdn');
+INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (5,'https://i.imgur.com/YN33wOW.png','Fundación Ignacio D','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','djnksdn');
+INSERT INTO `` (`id`,`imagen`,`nombre`,`private_token`,`public_token`) VALUES (6,'https://i.imgur.com/ei9yg83.jpeg','Hogar de Ancianos LT','APP_USR-1897691588852364-120221-3f923e24bb61c11ee1c1249b536718f6-570815386','djnksdn');
